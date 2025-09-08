67454c0d5db973527b8c2781a4cc420b4c65e525
AKociuba <127AKociuba@frhsd.com>
2025-09-08 10:34:15 -0400

    created project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
             System.out.println("Hi My Name Is Adrian");
-        }
+            System.out.println("Hi Adrian, my name is Alex");
     }
+}
